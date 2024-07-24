92db4660442649b7d711440bad9d1d0da0a1754e
Vince <vinnykipx@gmail.com>
2024-07-24 20:31:28 +0300

    bootstrapping

---
+++ components/AppContainer.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createTamagui, TamaguiProvider } from "tamagui";
+import { config } from "@tamagui/config/v3";
+import { useFonts } from "expo-font";
+const tamaguiConfig = createTamagui(config);
+
+// TypeScript types across all Tamagui APIs
+type Conf = typeof tamaguiConfig;
+declare module "@tamagui/core" {
+  interface TamaguiCustomConfig extends Conf {}
+}
+
+type Props = { children: React.ReactNode };
+export default function AppContainer({ children }: Props) {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  React.useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return <TamaguiProvider config={tamaguiConfig}>{children}</TamaguiProvider>;
+}
